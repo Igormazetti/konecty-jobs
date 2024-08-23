a3bc5138e13fc9f440c748b308aecc9008a847f0
Igor Mazetti <igor.mazetti@logithink.it>
2024-08-23 17:00:01 -0300

    feat: configured get products route

---
+++ fullstack/konecty-technical-test/app/api/products.ts
@@ -0,0 +1,15 @@
+import { products } from "../database/products";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const category = searchParams.get("category");
+
+  let result = products;
+
+  if (category) {
+    result = result.filter((item) => item.category === category);
+  }
+
+  return NextResponse.json(result);
+}
